6271f33dce316f6017cc8f3ad523d13d483f8811
mistrain <1053401058@qq.com>
2020-12-28 16:17:49 +0800

    master

--- src/pages/mine/index.jsx
+++ src/pages/mine/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import FootNav from '../../components/footNav'
+import './style.less'
 
 const Mine = props => {
   return (
     <div>
-      <FootNav/>
-      </div>
+      <FootNav />
+
+      
+    </div>
   )
 }
 
