af8656998e29eef7d032a044a5621d5da1c07255
mistrain <1053401058@qq.com>
2021-01-10 14:44:00 +0800

    master

--- src/pages/mine/options/set/index.jsx
+++ src/pages/mine/options/set/index.jsx
@@ -1,10 +1,10 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState} from 'react'
 import './style.less'
 import Header from '../../../../components/header'
 import * as loginActions from '../../../../actions/loginActions'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import {withRouter} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router-dom'
 
 const Set = props => {
   const [info, setInfo] = useState({})
@@ -15,7 +15,7 @@ const Set = props => {
         props.loginActions.changeToLoaded()
       })
   }, [props.loginActions])
-  
+
   const toResetPage = property => {
     props.history.push(`/mine/reset/${property}`)
   }
@@ -26,23 +26,23 @@ const Set = props => {
         <h4 style={{ fontWeight: 700, lineHeight: '0.6rem', textAlign: 'center' }}>设置</h4>
       </Header>
 
-      <div className="input-group">
-        <div className="input-item" onClick={() => toResetPage('修改头像')}>
-          <img src={info.img} alt="img" className="user-img"/>
+        <div className="input-group">
+          <div className="input-item" onClick={() => toResetPage('修改头像')}>
+            <img src={info.img} alt="img" className="user-img" />
+          </div>
+          <div className="input-item" onClick={() => toResetPage('修改账号')}>
+            账号 <p>{info.username}</p>
+          </div>
+          <div className="input-item" onClick={() => toResetPage('修改用户名')}>
+            用户名 <p>{info.user}</p>
+          </div>
+          <div className="input-item" onClick={() => toResetPage('个性签名')}>
+            个性签名 <p>{info.identity}</p>
+          </div>
+          <div className="input-item">
+            描述 <p>{info.msg}</p>
+          </div>
         </div>
-        <div className="input-item"  onClick={() => toResetPage('修改账号')}>
-          账号 <p>{info.username}</p>
-        </div>
-        <div className="input-item"  onClick={() => toResetPage('修改用户名')}>
-          用户名 <p>{info.user}</p>
-        </div>
-        <div className="input-item"  onClick={() => toResetPage('个性签名')}>
-          个性签名 <p>{info.identity}</p>
-        </div>
-        <div className="input-item">
-          描述 <p>{info.msg}</p>
-        </div>
-      </div>
     </Fragment>
   )
 }
